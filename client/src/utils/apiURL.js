bbed3e3ad0ef5b57f65ad9312a261783c0c70041
Gisele-123 <giselemigisha53@gmail.com>
2025-01-20 21:23:58 -0800

    receiveing data from backend hosted on render

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,4 +1,8 @@
-export const API_URL_USER = "http://localhost:9000/api/v1/users";
-export const API_URL_ACCOUNT = "http://localhost:9000/api/v1/accounts";
-export const API_URL_TRANSACTION = "http://localhost:9000/api/v1/transactions";
-// const url = "https://budget-tracker-backend.onrender.com";
+// export const API_URL_USER = "http://localhost:9000/api/v1/users";
+// export const API_URL_ACCOUNT = "http://localhost:9000/api/v1/accounts";
+// export const API_URL_TRANSACTION = "http://localhost:9000/api/v1/transactions";
+
+
+export const API_URL_USER = "https://wallet-tnr8.onrender.com/api/v1/users";
+export const API_URL_ACCOUNT = "https://wallet-tnr8.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION = "https://wallet-tnr8.onrender.com/api/v1/transactions";
